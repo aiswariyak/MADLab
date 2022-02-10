acff414c4d342a05f6eb388ccfee0df8bc0adebd
aiswariyak <74720701+aiswariyak@users.noreply.github.com>
2022-02-10 15:26:22 +0530

    Create MainActivity.java

---
+++ exercise11/MainActivity.java
@@ -0,0 +1,33 @@
+package com.aish.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void radioHandle(View view) {
+        boolean selected=((RadioButton)view).isChecked();
+        switch (view.getId())
+        {
+            case R.id.r1:
+                if(selected)
+                    Toast.makeText(this,"Male Selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.r2:
+                if(selected)
+                    Toast.makeText(this,"Female selected",Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+    }
+}
